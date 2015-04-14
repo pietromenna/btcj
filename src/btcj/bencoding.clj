629ccf076109d1b4329b9b34b1c3a3d8f69c3efb
Pietro F. Menna <pietromenna@yahoo.com>
2015-04-14 13:52:50 -0400

    bencode-int test passing

--- src/btcj/bencoding.clj
+++ src/btcj/bencoding.clj
@@ -1,13 +1,18 @@
 (ns btcj.bencoding)
 
-(def string-separator ":" )
+(def string-delimiter ":" )
+
+(def int-begin-delimiter "i" )
+
+(def int-end-delimiter "e" )
 
 (defn bencode-string [input-string] 
-  (str (count input-string) string-separator input-string))
+  (str (count input-string) string-delimiter input-string))
 
-(defn bdecode-string [encoded-string] ()))
+(defn bdecode-string [encoded-string] ())
 
-(defn bencode-int [input-int] ())
+(defn bencode-int [input-int] 
+  (str int-begin-delimiter input-int int-end-delimiter))
 
 (defn bdecode-int [encoded-int] ())
 
