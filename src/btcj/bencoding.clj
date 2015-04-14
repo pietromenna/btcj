2fb0ad820ab656520d9ada3bdd649c56c7e3e635
Pietro F. Menna <pietromenna@yahoo.com>
2015-04-14 14:58:41 -0400

    bencode-dict all but one passing

--- src/btcj/bencoding.clj
+++ src/btcj/bencoding.clj
@@ -10,6 +10,8 @@
 
 (def common-end-delimiter "e" )
 
+(defn bencode-commons [input])
+
 (defn bencode-string [input-string] 
   (str (count input-string) string-delimiter input-string))
 
@@ -18,20 +20,24 @@
 (defn bencode-int [input-int] 
   (str int-begin-delimiter input-int common-end-delimiter))
 
-(defn bencode-commons [input]
-  (cond 
-    (integer? input) (bencode-int input)
-    (string? input) (bencode-string input)
-    )
-  )
-
 (defn bdecode-int [encoded-int] ())
 
 (defn bencode-list [input-list] 
   (str list-begin-delimiter (apply str (map bencode-commons input-list)) common-end-delimiter))
 
+(defn bencode-commons [input]
+  (cond 
+    (vector? input) (bencode-list input)
+    (integer? input) (bencode-int input)
+    (string? input) (bencode-string input)
+    )
+  )
+
 (defn bdecode-list [encoded-list] ())
 
-(defn bencode-dict [input-dict] ())
+(defn bencode-dict [input-dict]
+  (let [bencode-key-value (fn [x] 
+                            (apply str (str (bencode-commons (key x)) (bencode-commons (val x))))) ] 
+  (str dict-begin-delimiter (apply str (map bencode-key-value input-dict)) common-end-delimiter)))
 
 (defn bdecode-dict [encoded-dict] ())
