30bc43114c3bbd09e85e7adaad5d203812e74a06
Pietro F. Menna <pietromenna@yahoo.com>
2015-05-31 16:16:48 -0400

    Now able to set info_hash in the format expected by the tracker communication

--- src/btcj/tracker_comm.clj
+++ src/btcj/tracker_comm.clj
@@ -1,6 +1,13 @@
-(ns btcj.tracker-comm)
+(ns btcj.tracker-comm
+  (:require [ring.util.codec :refer :all]))
 
 (defn sha-1 [string]
   (apply str (map (partial format "%02x")
                   (.digest (doto (java.security.MessageDigest/getInstance "SHA-1")
                              (.update (.getBytes string "ISO-8859-1")))))))
+
+(defn- sha1-to-byte-array [string]
+  (byte-array (take 20 (.toByteArray (BigInteger. string 16)))))
+
+(defn sha1-to-form-encoded [string]
+  (form-encode (new String (sha1-to-byte-array string) "ISO-8859-1") "ISO-8859-1"))
