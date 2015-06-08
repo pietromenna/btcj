aef2457dbcdeba24bac254f53e91f3b0bd55c776
Pietro F. Menna <pietromenna@yahoo.com>
2015-06-08 15:57:30 -0400

    Dediced to try to do requests manually

--- project.clj
+++ project.clj
@@ -8,6 +8,5 @@
   :profiles {:dev {;:global-vars {*warn-on-reflection* }
                    :dependencies [[midje "1.6.3"  :exclusions [org.clojure/clojure]]
                                   [org.clojars.runa/conjure "2.2.0"]
-                                  [clj-http "1.1.2"]
                                   [ring/ring-codec "1.0.0"]]}}
   :plugins [[lein-midje "3.1.3"]])
--- test/btcj/tracker_comm_test.clj
+++ test/btcj/tracker_comm_test.clj
@@ -26,7 +26,8 @@
         							:port 19999
         							:uploaded 0
         							:downloaded 0
-        							:compact 0 })
+        							:compact 0
+               						:event "started" })
 
 ; For a 20-byte hash of \x12\x34\x56\x78\x9a\xbc\xde\xf1\x23\x45\x67\x89\xab\xcd\xef\x12\x34\x56\x78\x9a,
 ; The right encoded form is %124Vx%9A%BC%DE%F1%23Eg%89%AB%CD%EF%124Vx%9A
